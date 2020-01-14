2bfdc88bbca32f60a496dd38355f387e9a2b8cef
zmercedes <zmercedes89@gmail.com>
2020-01-14 17:11:24 -0500

    feat: initialize AppCoordinator in AppDelegate

--- NYT_MartianNews/AppDelegate.swift
+++ NYT_MartianNews/AppDelegate.swift
@@ -12,10 +12,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        appCoordinator = AppCoordinator()
+        appCoordinator!.start()
+        
         return true
     }
 
