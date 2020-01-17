dd7570422d29d0173451798c4524f991142558e6
zmercedes <zmercedes89@gmail.com>
2020-01-17 11:10:02 -0500

    feat: add DataProvider
    This commit adds a DataProvider to the project. This will provide data to the app.

---
+++ NYT_MartianNews/Services/DataProvider.swift
@@ -0,0 +1,14 @@
+//
+//  DataProvider.swift
+//  NYT_MartianNews
+//
+//  Created by Zoilo Mercedes on 1/17/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import Foundation
+
+class DataProvider {
+    
+    
+}
