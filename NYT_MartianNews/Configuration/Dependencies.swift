f32e9151564f0525f6b98e78b4418940e0bb8e6d
zmercedes <zmercedes89@gmail.com>
2020-01-20 14:21:14 -0500

    feat: add Dependencies
    This commit adds the Dependencies class, which will contain data and user defaults.

---
+++ NYT_MartianNews/Configuration/Dependencies.swift
@@ -0,0 +1,14 @@
+//
+//  Dependencies.swift
+//  NYT_MartianNews
+//
+//  Created by Zoilo Mercedes on 1/20/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import Foundation
+
+open class Dependencies {
+    private lazy var userSettings = UserSettings()
+    lazy var dataProvider = DataProvider()
+}
