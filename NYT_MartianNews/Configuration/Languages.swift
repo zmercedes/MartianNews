3e2ccca69ba8ec850874e969281a0192df45cb86
zmercedes <zmercedes89@gmail.com>
2020-01-20 13:11:39 -0500

    feat: add Language enum
    This commit adds a language enumeration.

---
+++ NYT_MartianNews/Configuration/Languages.swift
@@ -0,0 +1,14 @@
+//
+//  Languages.swift
+//  NYT_MartianNews
+//
+//  Created by Zoilo Mercedes on 1/20/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import Foundation
+
+enum Languages{
+    case English
+    case Martian
+}
