3c61198d405c3f71469b77ccdfbf640b729e38e8
zmercedes <zmercedes89@gmail.com>
2020-01-21 11:24:18 -0500

    feat: add NibLoadableView
    This commit adds NibLoadableView protocol, which will be used with TableViewCells.

---
+++ NYT_MartianNews/Protocols/NibLoadableView.swift
@@ -0,0 +1,22 @@
+//
+//  NibLoadableView.swift
+//  NYT_MartianNews
+//
+//  Created by Zoilo Mercedes on 1/21/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+protocol NibLoadableView: class {
+    static var nibName: String { get }
+    
+}
+
+extension NibLoadableView where Self: UIView {
+    
+    static var nibName: String {
+        return String(describing: self)
+    }
+    
+}
