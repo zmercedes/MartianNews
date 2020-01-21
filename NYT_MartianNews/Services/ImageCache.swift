d02a97397f94caaa871b1c1201ca4dc59c3480fb
zmercedes <zmercedes89@gmail.com>
2020-01-21 17:16:44 -0500

    feat: add ImageCache
    This commit adds ImageCache which will store images to avoid having to redownload images each time.

---
+++ NYT_MartianNews/Services/ImageCache.swift
@@ -0,0 +1,32 @@
+//
+//  ImageCache.swift
+//  NYT_MartianNews
+//
+//  Created by Zoilo Mercedes on 1/21/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+final class ImageCache {
+    private var images: [String: UIImage] = [:]
+    
+    func storeImage(url: String, newImage: UIImage) {
+        images[url] = newImage
+    }
+    
+    func getImage(url: String) -> UIImage? {
+        if let image = images[url] {
+            return image
+        } else {
+            return nil
+        }
+    }
+    
+    func clear(){
+        for key in images.keys {
+            images[key] = nil
+        }
+    }
+
+}
