6e247352861f0f0582ff03c113a2aa307448bd35
zmercedes <zmercedes89@gmail.com>
2020-01-21 11:26:32 -0500

    feat: add ReusableView
    This commit adds ReusableView, which will be used with TableViewCells.

---
+++ NYT_MartianNews/Protocols/ReusableView.swift
@@ -0,0 +1,23 @@
+//
+//  ReusableView.swift
+//  NYT_MartianNews
+//
+//  Created by Zoilo Mercedes on 1/21/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView: class {
+    static var reuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+    
+}
+
+extension UITableViewCell: ReusableView, NibLoadableView {}
