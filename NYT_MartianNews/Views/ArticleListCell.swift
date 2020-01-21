f7418779bc91d7dbc85f8ada9f4cc2660dd62062
zmercedes <zmercedes89@gmail.com>
2020-01-21 10:22:20 -0500

    feat: add ArticleListCell
    This commit adds ArticleListCell and its corresponding view.

---
+++ NYT_MartianNews/Views/ArticleListCell.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleListCell.swift
+//  NYT_MartianNews
+//
+//  Created by Zoilo Mercedes on 1/21/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+class ArticleListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func prepareForReuse() {
+        
+    }
+    
+}
