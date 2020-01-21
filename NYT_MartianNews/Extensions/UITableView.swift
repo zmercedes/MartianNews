0b30adc5a28004e50ff3fb375ec1bc24c6bfaf20
zmercedes <zmercedes89@gmail.com>
2020-01-21 11:33:28 -0500

    feat: extend UITableView
    This commit extends UITableView to use NibLoadableView and ReusableView in order to quickly register/dequeue cells for TableViews.

---
+++ NYT_MartianNews/Extensions/UITableView.swift
@@ -0,0 +1,25 @@
+//
+//  UITableView.swift
+//  NYT_MartianNews
+//
+//  Created by Zoilo Mercedes on 1/21/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func register<T: UITableViewCell>(_: T.Type) where T: ReusableView, T: NibLoadableView {
+        let nib = UINib(nibName: T.nibName, bundle: nil)
+        self.register(nib, forCellReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
+        guard let cell = self.dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+        }
+        return cell
+    }
+    
+}
