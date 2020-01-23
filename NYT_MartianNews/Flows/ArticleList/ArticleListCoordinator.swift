18d041b7e15e421746292e2ad149645386775288
zmercedes <zmercedes89@gmail.com>
2020-01-22 20:14:39 -0500

    feat: add route to ArticleViewController from ArticleListCoordinator
    This commit adds a route to ArticleViewController from ArticleListCoordinator, and makes it ArticleListViewControllerDelegae compliant.

--- NYT_MartianNews/Flows/ArticleList/ArticleListCoordinator.swift
+++ NYT_MartianNews/Flows/ArticleList/ArticleListCoordinator.swift
@@ -19,6 +19,8 @@ class ArticleListCoordinator: Coordinator {
     private let dataSource : ArticleListDataSource
     private let disposeBag = DisposeBag()
     
+    private var currentArticle: Article?
+    
     init(dependencies: Dependencies, navigation: UINavigationController) {
         self.dependencies = dependencies
         self.navigationController = navigation
@@ -29,6 +31,7 @@ class ArticleListCoordinator: Coordinator {
         print("navigated to Article List")
         let viewController = ArticleListViewController(dataSource: self.dataSource)
         viewController.title = "The Martian News"
+        viewController.delegate = self
         navigationController.setViewControllers([viewController], animated: false)
         dependencies.dataProvider.articles.observe { articles in
             let language = self.dependencies.dataProvider.language.value
@@ -39,8 +42,17 @@ class ArticleListCoordinator: Coordinator {
     func navigate(to destination: ArticleListCoordinator.Destination) {
         switch destination {
         case .selectedArticle:
-            print("Navigated to selected article")
+            let language = dependencies.dataProvider.language.value
+            let image = dependencies.imageCache.getImage(url: currentArticle!.imageURL)!
+            let viewController = ArticleViewController(article: currentArticle!, language: language, image: image)
+            navigationController.pushViewController(viewController, animated: true)
         }
     }
-    
+}
+
+extension ArticleListCoordinator: ArticleListViewControllerDelegate {
+    func viewArticle(row: Int) {
+        currentArticle = self.dependencies.dataProvider.articles.value[row]
+        navigate(to: .selectedArticle)
+    }
 }
