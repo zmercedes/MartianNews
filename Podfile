2d5e17e87869974b59ea523b29f333b5b6b24b9f
zmercedes <zmercedes89@gmail.com>
2020-01-27 12:03:11 -0500

    feat: add cocoapods + swiftlint
    This commit adds cocoapods dependency management and swiftlint for style enforcement.

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '12.0'
+
+target 'NYT_MartianNews' do
+  pod 'SwiftLint', '0.31'
+end
---
+++ Pods/Target Support Files/Pods-NYT_MartianNews/Pods-NYT_MartianNews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NYT_MartianNews : NSObject
+@end
+@implementation PodsDummy_Pods_NYT_MartianNews
+@end
