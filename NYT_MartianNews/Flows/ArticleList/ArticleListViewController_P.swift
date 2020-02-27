02b543e1cf933173430cfd405b86860d8107d928
zmercedes <zmercedes89@gmail.com>
2020-02-27 14:11:29 -0500

    feat: add ArticleListViewController_P
    This commit adds a programmatic version of ArticleListViewController.

---
+++ NYT_MartianNews/Flows/ArticleList/ArticleListViewController_P.swift
@@ -0,0 +1,58 @@
+//
+//  ArticleListViewController_P.swift
+//  NYT_MartianNews
+//
+//  Created by Zoilo Mercedes on 2/27/20.
+//  Copyright © 2020 Zoilo Mercedes. All rights reserved.
+//
+
+import UIKit
+
+class ArticleListViewController_P: UIViewController {
+
+    var tableView: UITableView!
+    private let dataSource: ArticleListDataSource
+    private let viewArticle: ((Int) -> Void)
+
+    init(dataSource: ArticleListDataSource, viewArticle: @escaping ((Int) -> Void)) {
+        self.dataSource = dataSource
+        self.viewArticle = viewArticle
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func loadView() {
+        super.loadView()
+        view = UIView()
+        view.backgroundColor = .groupTableViewBackground
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+
+        tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+        tableView.backgroundColor = .groupTableViewBackground
+        view.addSubview(tableView)
+        self.dataSource.tableView = self.tableView
+        tableView.register(ArticleListCell.self)
+        tableView.dataSource = self.dataSource
+        tableView.delegate = self
+        
+        let guide = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: guide.topAnchor, constant: 4),
+            tableView.trailingAnchor.constraint(equalTo: guide.trailingAnchor),
+            tableView.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: guide.bottomAnchor)
+        ])
+    }
+}
+
+extension ArticleListViewController_P: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        viewArticle(indexPath.row)
+        tableView.deselectRow(at: indexPath, animated: false)
+    }
+}
