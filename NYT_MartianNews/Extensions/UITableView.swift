c74184a293bf84130ca07cd591c3e7fa42d9edbe
zmercedes <zmercedes89@gmail.com>
2020-02-27 14:04:13 -0500

    refactor: UITableView extensions
    This commit changes the redundant conditions in UITableView extensions.

--- NYT_MartianNews/Extensions/UITableView.swift
+++ NYT_MartianNews/Extensions/UITableView.swift
@@ -10,12 +10,12 @@ import UIKit
 
 extension UITableView {
 
-    func register<T: UITableViewCell>(_: T.Type) where T: ReusableView, T: NibLoadableView {
+    func register<T: UITableViewCell>(_: T.Type)  {
         let nib = UINib(nibName: T.nibName, bundle: nil)
         self.register(nib, forCellReuseIdentifier: T.reuseIdentifier)
     }
 
-    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T  {
         guard let cell = self.dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
         }
