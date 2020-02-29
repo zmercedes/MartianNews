d55c1d70c4aa34dc0c9783ce1c4d117fb305bb14
zmercedes <zmercedes89@gmail.com>
2020-02-29 00:48:59 -0500

    refactor: UITableView extensions
    This commit changes UITableView extensions to have registering for both nib based and programmatic cells.

--- NYT_MartianNews/Extensions/UITableView.swift
+++ NYT_MartianNews/Extensions/UITableView.swift
@@ -10,12 +10,16 @@ import UIKit
 
 extension UITableView {
 
-    func register<T: UITableViewCell>(_: T.Type)  {
-        let nib = UINib(nibName: T.nibName, bundle: nil)
+    func register<T: UITableViewCell>(_: T.Type) {
+        self.register(T.self, forCellReuseIdentifier: T.reuseIdentifier)
+    }
+
+    func registerWithNib<T: UITableViewCell>(_: T.Type)  {
+        let nib = UINib(nibName: T.reuseIdentifier, bundle: nil)
         self.register(nib, forCellReuseIdentifier: T.reuseIdentifier)
     }
 
-    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T  {
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
         guard let cell = self.dequeueReusableCell(withIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
         }
